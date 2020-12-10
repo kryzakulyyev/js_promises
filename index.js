86743dfbd10c68732d5a5e1e9054d7648377659e
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2020-12-09 23:54:25 -0800

    Added files

---
+++ index.js
@@ -0,0 +1,8 @@
+// let orderPromise= require ("./1-Promise-Object.js")
+// console.log(orderPromise)
+// require("./2-setTimeout-Function.js")
+// require("./3-onFulfilled-and-onRejected-Functions.js")
+// require("./4-catch-Function.js")
+require("./5-Chaining-Multiple-Promises.js")
+// require("./6-Common-Mistakes.js")
+// require("./7-promiseAll-Funciton.js")
